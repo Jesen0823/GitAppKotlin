81e9da4126763a25fe52f6a9d9159e1c1324ade9
Jesen0823 <xie_stacol@163.com>
2021-03-21 04:29:16 +0800

    SDL中使用主题

--- app/src/main/java/com/jesen/cod/gitappkotlin/view/fragments/subfragments/AboutFragment.kt
+++ app/src/main/java/com/jesen/cod/gitappkotlin/view/fragments/subfragments/AboutFragment.kt
@@ -3,15 +3,16 @@ package com.jesen.cod.gitappkotlin.view.fragments.subfragments
 /**
  * 关于页面，采用Anko Dsl布局
  * */
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import cn.carbs.android.avatarimageview.library.AvatarImageView
 import com.jesen.cod.gitappkotlin.R
+import com.jesen.cod.gitappkotlin.utils.loadWithGlide
 import com.jesen.cod.gitappkotlin.utils.markdownText
 import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.sdk15.listeners.onClick
 import org.jetbrains.anko.support.v4.UI
 import org.jetbrains.anko.support.v4.nestedScrollView
@@ -34,24 +35,35 @@ class AboutFragmentUI : AnkoComponent<AboutFragment> {
         return ui.apply {
             nestedScrollView {
                 verticalLayout {
+
+                    avatarImageView {
+                        //为了演示引用第三方库的Theme支持
+                        setTextAndColorSeed("Jef", "123")
+                    }.lparams(width = dip(30), height = dip(30)) {
+                        gravity = Gravity.CENTER_HORIZONTAL
+                    }
+
                     imageView {
                         imageResource = R.mipmap.ic_launcher_round
                     }.lparams(width = wrapContent, height = wrapContent) {
                         gravity = Gravity.CENTER_HORIZONTAL
                     }
-                    textView("GitHub") {
+
+                    // 使用主题
+                    themedTextView("GitHub", R.style.detail_title) {
                         textColor = R.color.colorPrimary
                         padding = dip(10)
                     }.lparams(width = wrapContent, height = wrapContent) {
                         gravity = Gravity.CENTER_HORIZONTAL
                     }
 
-                    textView("Jesen0823") {
+                    themedTextView("Jesen0823", R.style.detail_description) {
                         textColor = R.color.colorPrimary
                     }.lparams(width = wrapContent, height = wrapContent) {
                         gravity = Gravity.CENTER_HORIZONTAL
                     }
 
+                    // 没有使用主题
                     textView(R.string.open_source_license) {
                         textColor = R.color.colorPrimary
 
@@ -78,4 +90,15 @@ class AboutFragmentUI : AnkoComponent<AboutFragment> {
             }
         }.view
     }
+}
+
+/***
+ * 为了演示引用第三方库的控件如何使用
+ * Copy from:package org.jetbrains.anko
+ * 修改返回类型和参数
+ * */
+inline fun ViewManager.avatarImageView(): AvatarImageView = avatarImageView() {}
+
+inline fun ViewManager.avatarImageView(init: (@AnkoViewDslMarker AvatarImageView).() -> Unit): AvatarImageView {
+    return ankoView({ ctx: Context -> AvatarImageView(ctx) }, theme = 0) { init() }
 }
